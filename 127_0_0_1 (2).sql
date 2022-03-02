259e093180345ce708dc53651debc17a2869925e
shifan-sadique <67517134+shifan-sadique@users.noreply.github.com>
2022-03-02 10:43:19 +0530

    database

---
+++ 127_0_0_1 (2).sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 02, 2022 at 06:12 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library`
+--
+CREATE DATABASE IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `library`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_request`
+--
+
+CREATE TABLE `book_request` (
+  `Request ID` int(10) NOT NULL,
+  `Book name` int(50) NOT NULL,
+  `author name` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_taken`
+--
+
+CREATE TABLE `book_taken` (
+  `tid` int(10) NOT NULL,
+  `Book id` int(10) NOT NULL,
+  `book name` varchar(50) NOT NULL,
+  `author` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book_taken`
+--
+
+INSERT INTO `book_taken` (`tid`, `Book id`, `book name`, `author`) VALUES
+(101, 201, 'Wings of Fire', 'apj adbul kalam'),
+(102, 202, 'two states', 'chetan bhagat'),
+(101, 205, 'And the Mountains echoed', 'Khlaid Hosseini');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `tid` int(10) NOT NULL,
+  `bookid` int(10) NOT NULL,
+  `bookname` varchar(50) NOT NULL,
+  `author` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`tid`, `bookid`, `bookname`, `author`) VALUES
+(101, 501, 'the motorcycle diaries', 'Che Guevara'),
+(102, 502, 'wings of fire', 'apj adbul kalam'),
+(103, 503, 'the alchemist', 'paulo coelho'),
+(104, 504, 'aadujeevitham', 'benyamin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book_request`
+--
+ALTER TABLE `book_request`
+  ADD PRIMARY KEY (`Request ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book_request`
+--
+ALTER TABLE `book_request`
+  MODIFY `Request ID` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
